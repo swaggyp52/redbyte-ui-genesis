75eb2c38f9d372961459fa672f448cc23ec40ed6
swaggyp52 <connorangiel@gmail.com>
2025-12-01 15:54:34 -0500

    Fix Tailwind not loading

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 ﻿/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./index.html', './src/**/*.{ts,tsx,js,jsx}'],
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
