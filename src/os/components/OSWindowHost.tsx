244333a439f37b2ba3dc42ac7e4a25534ca328ac
swaggyp52 <connorangiel@gmail.com>
2025-12-01 00:31:56 -0500

    Fix: correct OSWindow import path

--- src/os/components/OSWindowHost.tsx
+++ src/os/components/OSWindowHost.tsx
@@ -1,14 +1,14 @@
 ﻿import React from "react";
 import { useWindowManager } from "../state/windowStore";
-import OSWindow from "../components/Window";
+import OSWindow from "../../components/os/Window";
 import "./oswindowhost.css";
 
 export default function OSWindowHost() {
-  const windows = useWindowManager(s => s.windows);
+  const windows = useWindowManager((s) => s.windows);
 
   return (
     <div className="rb-window-host">
-      {windows.map(win => (
+      {windows.map((win) => (
         <OSWindow key={win.id} win={win} />
       ))}
     </div>
