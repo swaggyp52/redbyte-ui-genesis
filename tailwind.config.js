5ee232a05eb0bf3ff7eab31eacfa55f6d0f8e60d
swaggyp52 <connorangiel@gmail.com>
2025-12-01 21:51:20 -0500

    SUPERMEGA v3: full autoheal, config reset, BOM purge, build

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
-﻿export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+export default {
+  content: ['./index.html','./src/**/*.{js,jsx,ts,tsx}'],
   theme: { extend: {} },
   plugins: [],
-};
+}
