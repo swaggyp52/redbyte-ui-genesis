c54a98005392986eeae1bd3a365556108eb26262
swaggyp52 <connorangiel@gmail.com>
2025-12-02 16:23:56 -0500

    fix: force cinematic boot + UniverseOrb always shows

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-﻿import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+﻿import React from "react";
+import ReactDOM from "react-dom/client";
+import BootScreen from "./os/boot/BootScreen";
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+const root = ReactDOM.createRoot(document.getElementById("root")!);
+
+function RootController() {
+  const [booted, setBooted] = React.useState(false);
+
+  if (!booted) {
+    return <BootScreen onDone={() => setBooted(true)} />;
+  }
+
+  return <App />;
+}
+
+root.render(<RootController />);
