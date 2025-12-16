7fa7aa85c178b29f2b5b55d8a1bd604a9c31907e
swaggyp52 <connorangiel@gmail.com>
2025-12-16 16:59:51 -0500

    Playground: toggle launcher via Ctrl+K

--- apps/playground/src/main.tsx
+++ apps/playground/src/main.tsx
@@ -6,9 +6,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Shell } from '@redbyte/rb-shell';
 import './index.css';
+import { LauncherSearchPanel } from './LauncherSearchPanel';
+import { useLauncherSearchToggle } from './useLauncherSearchToggle';
+
+export function PlaygroundRoot() {
+  const { isLauncherOpen, closeLauncher, shellRef } = useLauncherSearchToggle();
+
+  return (
+    <div ref={shellRef} tabIndex={-1} className="relative h-full" data-testid="playground-shell">
+      <Shell />
+      {isLauncherOpen && <LauncherSearchPanel onClose={closeLauncher} />}
+    </div>
+  );
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Shell />
+    <PlaygroundRoot />
   </React.StrictMode>
 );
